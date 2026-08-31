da478203699e7af374d352b97d9e939caef634f8
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2026-08-31 15:15:27 +0900

    ブルースクリーン出るようにした

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,34 @@
 #include <Windows.h>
 
+#include "KamataEngine.h"
+
+using namespace KamataEngine;
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+
+	// エンジンの初期化
+	Initialize(L"ガリリとデブブ");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	Finalize();
+
 	return 0;
 }
